96047d235f977c0718fc275cd54977fa16f6db9f
mitsuki <e165732@ie.u-ryukyu.ac.jp>
2016-12-21 09:51:56 +0900

    ステップ１終了

---
+++ src/main/java/jp/ac/uryukyu/ie/e165732/Enemy.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e165732;
+
+public class Enemy extends LivingThing{
+    public Enemy (String name, int hitPoint, int attack){
+        super(name, hitPoint, attack);
+    }
+
+    @Override
+    public void wounded(int damage){
+        setHitPoint(getHitPoint()-damage);
+        if( getHitPoint() <= 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+}
